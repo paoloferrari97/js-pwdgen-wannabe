d02ba2613a66174f6e448c665a919e27ec5070e4
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-04-22 14:45:38 +0200

    Stampo le variabili anche nella console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
 //nome utente
 var nameUser = prompt("Qual è il tuo nome?");
 
+console.log(nameUser);
+
 //cognome utente
 var lastNameUser = prompt("Qual è il tuo cognome?");
+
+console.log(lastNameUser);
 
 //colore preferito utente
 var favColorUser = prompt("Qual è il tuo colore preferito?");
 
+console.log(favColorUser);
+
 //password generata (nomecognomecolore21)
 var password = nameUser + lastNameUser + favColorUser + 21;
+
+console.log(password);
+
 document.getElementById("password_generata").innerHTML = password;
