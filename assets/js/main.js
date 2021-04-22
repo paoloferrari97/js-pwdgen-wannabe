6b85a6457de05fbd1875436f6177108ec5f2a595
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-04-22 19:21:35 +0200

    Rendo password minuscola e abbelisco con css

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,25 @@ var nameUser = prompt("Qual è il tuo nome?");
 
 console.log(nameUser);
 
+document.getElementById("nome_utente").innerHTML = "Il tuo nome è:" + " " + nameUser;
+
 //cognome utente
 var lastNameUser = prompt("Qual è il tuo cognome?");
 
 console.log(lastNameUser);
+
+document.getElementById("cognome_utente").innerHTML = "Il tuo cognome è:" + " " + lastNameUser;
 
 //colore preferito utente
 var favColorUser = prompt("Qual è il tuo colore preferito?");
 
 console.log(favColorUser);
 
+document.getElementById("colore_utente").innerHTML = "Il tuo colore preferito è:" + " " + favColorUser;
+
 //password generata (nomecognomecolore21)
 var password = nameUser + lastNameUser + favColorUser + 21;
 
 console.log(password);
 
-document.getElementById("password_generata").innerHTML = password;
+document.getElementById("password_generata").innerHTML = "La tua password è:" + " " + password.toLowerCase();
